2ec4e11b8db32b8c19c18aa03eaaa3ad9a3c4f02
mjgoins <mjgoins@9420e8cb-7f0d-48f1-80e1-2dafae88fb5b>
2009-12-16 22:08:44 +0000

    Tagging greenmap_006-mjgoins.1

--- themes/greenmapnew/page.tpl.php
+++ themes/greenmapnew/page.tpl.php
@@ -104,10 +104,14 @@ return;
 				<?php print $messages ?>
 				<?php print $content; ?>
 			
-</div>		
 
-
-</div>
+	<div id="footer_gh">
+		<div id="bottomleft">&nbsp;</div>
+		<div id="bottomright">&nbsp;</div>
+		<?php print $footerlinks; ?>
+		&nbsp;
+	</div>
+		</div>
 </div>		
 
 
