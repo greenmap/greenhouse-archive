4df895bf02379c37a39bea90455651ee46d62020
mjgoins <mjgoins@9420e8cb-7f0d-48f1-80e1-2dafae88fb5b>
2010-03-19 19:28:46 +0000

    Tagging greenmap_008

---
+++ tags/greenmap_008/modules/contrib/ecommerce/tangible/README.txt
@@ -0,0 +1,23 @@
+Product Module  : TANGIBLE
+Original Author : Matt Westgate
+Settings        : 
+
+********************************************************************
+DESCRIPTION:
+
+Creates shippable products for ecommerce.  Optional features 
+includes inventory control and availability estimates.
+
+********************************************************************
+
+See MAINTAINERS.txt for more maintenance info.
+See README.txt (in E-Commerce root) for other info.
+
+
+
+
+
+
+
+
+
---
+++ tags/greenmap_008/modules/contrib/greenmap/temp/registrationtext.php
@@ -0,0 +1,24 @@
+<?php drupal_add_js('misc/collapse.js'); ?>
+<p>We welcome your participation in this vibrant global movement. We hope that 
+  you have reviewed some of the profiles in the Maps section, and read through 
+  the Introduction and FAQ in this section. </p>
+<p>Now, Get Ready! Here are the Five Steps</p>
+<p>Step 1: Check if a Green Map project is already underway in your area (Check 
+  the List). If it's active, contact the Mapmaker and see if you can get involved. 
+  If there are no existing Green Map projects or if your specific project scale 
+  is different, you can begin the registration process.</p>
+<p>Step 2: Read over the <?php print l(t('Terms and Conditions'),'participate/mapmakeragreementplain'); ?> (link opens in new window). </p>
+<p>Step 3 (optional): Download the offline registration documents. To register 
+  as a Green Mapmaker requires a lot of information. If you wish to discuss the 
+  information with your project partners you can download the documents and complete 
+  them offline. Then come back to the website and continue the registration process.</p>
+<fieldset class="collapsible collapsed"><label>Offline Registration Documents</label>
+<a href="<?php print file_create_url('Registration_EN.zip'); ?>">English</a> - <a href="<?php print file_create_url('Registration_CN_Traditional.zip'); ?>">Chinese Traditional</a>
+ - <a href="<?php print file_create_url('Registration_Simp_CN.zip'); ?>">Simplified Chinese</a> - <a href="<?php print file_create_url('Registration_ES.zip'); ?>">Español</a>
+  - <a href="<?php print file_create_url('Registration_ID.zip'); ?>">Indonesian</a> - <a href="<?php print file_create_url('Registration_JP.zip'); ?>">Japanese</a>
+</fieldset>
+<p>Step 4: Complete all fields in the form below and click the 'Register' button.</p>
+<p>Step 5: Once you have submitted the form you will receive an email with 
+  your login details and will be taken to the next page to add the rest of your details. 
+  You can save your information at any point, and come back to make changes and 
+  finish the process.</p>
