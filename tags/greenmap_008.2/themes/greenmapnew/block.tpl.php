df30dffd8bd6d416897ed256a97310aa99cd3a9f
mjgoins <mjgoins@9420e8cb-7f0d-48f1-80e1-2dafae88fb5b>
2010-03-22 17:57:46 +0000

    Tagging greenmap_008.2

---
+++ tags/greenmap_008.2/themes/greenmapnew/block.tpl.php
@@ -0,0 +1,6 @@
+<!--block.tpl.php-->
+<div class="block block-<?php print $block->module; ?>" id="block-<?php print $block->module; ?>-<?php print $block->delta; ?>">
+<h1><?php print $block->subject; ?></h1>
+<div class="content"><?php print $block->content; ?></div>
+</div>
+<!--/block.tpl.php-->
