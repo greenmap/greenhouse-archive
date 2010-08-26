361d849f7d09a7a5015d38ef186a13377f7a875d
te <te@9420e8cb-7f0d-48f1-80e1-2dafae88fb5b>
2010-08-26 19:00:43 +0000

    Adding new theme utilized by by_referrer module for usage within iPhone App's in-app browser

---
+++ themes/iphoneappreferral/page.tpl.php
@@ -0,0 +1,54 @@
+<?php 
+global $base_url;
+global $i18n_langpath; ?>
+
+<?php
+if ($_GET[theme] == 'simple') { include('page-simple.tpl.php');
+return;
+}
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<!--page.tpl.php-->
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+  <?php print $styles ?>
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+<meta name="author" content="Original design by Andreas Viklund - http://andreasviklund.com / Ported by Matt Koglin - http://antinomia.comn / restyled by Thomas Turnbull - http://wwww.thomasturnbull.com , Te Baybute - http://tebaybute.net , and Akiko Rokube http://rokube.com / for http://www.greenmap.org" />
+
+
+</head>
+
+
+			<div id="content">
+				<?php
+				if (!$frontpage) {
+				  print $breadcrumb;
+				}
+				?>
+				<?php // print $breadcrumb ?>
+				<?php if ($title) { ?><h1><?php print $title ?></h1><?php } ?>
+				<?php print $help ?>
+				<?php print $messages ?>
+				<?php print $content; ?>
+			
+</div>		
+
+
+	
+
+	
+	<?php print $closure ?>
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-418876-2");
+pageTracker._trackPageview();
+} catch(err) {}</script> 
+</body>
+</html>
+<!--/page.tpl.php-->
