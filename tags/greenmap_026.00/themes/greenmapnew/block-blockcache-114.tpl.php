de33326bd6f50b96bb077b4b57938c2a1ef74d0f
thomasturnbull <thomasturnbull@9420e8cb-7f0d-48f1-80e1-2dafae88fb5b>
2011-09-24 21:11:01 +0000

    part of massive module upgrade

---
+++ tags/greenmap_026.00/themes/greenmapnew/block-blockcache-114.tpl.php
@@ -0,0 +1,6 @@
+<!--block-blockcache-114.tpl.php-->
+<div class="block block-<?php print $block->module; ?>" id="block-<?php print $block->module; ?>-<?php print $block->delta; ?>">
+<h1><?php print $block->subject; ?></h1>
+<div class="content">Impacts: <?php print $block->content; ?></div>
+</div>
+<!--/block-blockcache-114.tpl.php-->
